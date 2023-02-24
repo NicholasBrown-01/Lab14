753036c72734a66d08044e0bad104fdcdddb07f4
Nicholas <nicholasbrown.us@gmail.com>
2023-02-23 19:22:20 -0600

    Updated CSS highlight boxes

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+
 };
 
 Cart.prototype.removeItem = function(item) {
